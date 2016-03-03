8f32cec4fb597957f158c676b56216520fa9d535
Haokun Xing <13218036521@163.com>
2016-03-03 16:45:22 +0800

    1st

---
+++ routes/login.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+
+router.post('/', function(req, res, next) {
+  // res.render('res');
+  // res.send(req.body.admin + req.body.password);
+  // res.send("sss");
+  
+});
+
+module.exports = router;
