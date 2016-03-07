dd2bac44f8eb771a1fbed8008a4d607fd3cbc0ad
EastblueOkay <xuanxuan_lan@126.com>
2016-03-07 11:37:24 +0800

    add admin_index.ejs general

--- routes/admin_index.js
+++ routes/admin_index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('admin_index');
+  	res.render('admin_index');
+});
+
+router.post('/admin_index', function(req, res, next){
+
+	//验证用户名和密码
+	//session
+	//数据库读取各类信息
+	res.render('admin_general');
 });
 
 module.exports = router;
