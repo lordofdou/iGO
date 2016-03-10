1ea7c76517b1cb236a5703ec3b5351d72e2dcc21
EastblueOkay <xuanxuan_lan@126.com>
2016-03-10 15:24:47 +0800

    try complete activity.

--- public/javascripts/admin_activity.js
+++ public/javascripts/admin_activity.js
@@ -1,15 +1,30 @@
 $(document).ready(function(){
 
 
+	//显示相应的图片布局
+	for(var i = 0; i<$('.pagination li').length; i++){
+		if($($('.pagination li')[i]).hasClass("active")){
+			var selectedClass = $($('.pagination li')[i]).attr('activity_title');
+			$('.square').hide();
+			$('.' + selectedClass).show();
+		}
+	}
+
+
+	//顶部segment切换
 	$('.pagination li').click(function(){
 		$('.pagination li').removeClass("active");
 		$(this).addClass("active");
 
-		var title = $(this).attr("title");
-
-		//隐藏
+		var selectedClass = $(this).attr("activity_title");
 		$('.square').hide();
-		$('.' + title).show();
+		$('.' + selectedClass).show();
 
+	});
+
+
+	//图片上传
+	$('input[type="file"]').change(function(){
+		$("#uploadForm").submit();
 	});
 });
--- routes/admin_activity.js
+++ routes/admin_activity.js
@@ -1,19 +1,103 @@
 var express = require('express');
 var router = express.Router();
+var sql = require('./sql');
+var formidable = require('formidable');
+var fs = require('fs');
+var AVATAR_UPLOAD_FOLDER = '/activityUpload/';
+var path=require('path');
+var	StringDecoder = require('string_decoder').StringDecoder;
+var	EventEmitter = require('events').EventEmitter;
+var	util=require('util');
 
 
-router.get('/', function(req, res, next) {
-	//没有登录无法进入此页面
+function loginValidate(req, res){
+//没有登录无法进入此页面
 	if(!req.session.username){
 		res.redirect('/');
 		return;
 	}
-
-
-	//读取数据库中 活动数据  
-
+}
+router.get('/', function(req, res, next) {
+	loginValidate(req, res);
+	sql.connect();
+	//读取数据库活动类别名称
+	sql.activitySelectCategoryName(function(err, categorys){
+		if(err){
+			res.render('fail', {title : "获取数据失败", message: "数据库活动分类数据出现丢失"});
+			return;
+		}
 	
-  	res.render('admin_activity', {admin_name: req.session.username});
+		//读取数据库中 活动数据  
+		sql.activitySelectAllRecord(function(err, results){
+			if(err){
+				res.render('fail', {title : "获取数据失败", message: "活动数据库出现错误"});
+				return;
+			}
+			console.log(results);
+			res.render('admin_activity', {admin_name: req.session.username, admin_activity_category: categorys, admin_activity_pops : results});
+		});
+	
+	});
+});
+
+router.post('/uploadPic', function(req, res, next){
+	loginValidate(req, res);
+
+	EventEmitter.call(this);
+	var form = new formidable.IncomingForm(); 
+    form.path = __dirname + '/../public' + AVATAR_UPLOAD_FOLDER;	 //设置上传目录
+
+	form.parse(req,function(error, fields, files){
+		if (error) {
+	      res.render('fail', {title : "上传失败", message: err});
+	      return;		
+	    } 
+
+	    var position;
+	    for(var key in files){
+	    	if(files[key]["size"]){
+	    		position = key;
+	    		break;
+	    	}
+	    }
+
+		var extName = 'png';  //后缀名
+
+		var avatarName = Math.random() + '.' + extName;
+	    var newPath = form.path + avatarName;
+
+	    fs.renameSync(files[key]["path"], newPath);  //重命名		
+
+	    //将位置、类别写入数据库	
+	    var pid = parseInt(files[key]["name"].substring(0, files[key]["name"].lastIndexOf(".")));
+	    if(!pid){
+	    	pid = 0;
+	    }
+	    var popid = fields['popid'];
+	    var postion = key;
+	    var url = newPath.substring(newPath.lastIndexOf("activityUpload"), newPath.length);
+	    var isVisiable = 1;
+
+	    var insertArray = new Array();
+	    insertArray.pid = pid;
+	    insertArray.popid = popid;
+	    insertArray.position = position;
+	    insertArray.url = url;
+	    insertArray.isVisiable = isVisiable;
+	    sql.activityInsertARecord(insertArray, function(err, results){
+	    	if(err){
+	    		res.render('fail', {title : "上传失败", message: "活动数据库出错"});
+	     		return;	
+	    	}
+
+	   		res.redirect('/admin_activity');
+	    });
+
+
+    });
+
+	return;
+
 });
 
 module.exports = router;
